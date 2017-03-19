e0e9d8c9f600d21828ea071305a5ebbd1aaf0242
diahevyka <diah.hevyka@gmail.com>
2017-03-19 13:59:47 +0700

    qwe

--- relasi.cpp
+++ relasi.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 #include "relasi.h"
+using namespace std;
+
+address_relasi alokasiRelasi( infotype_relasi C)
+{
+
+}
+
+void dealokasiRelasi(adddress_relasi &P)
+{
+
+}
+
+void insertFirstRelasi(address_mhs P, address_relasi L, infotype_relasi R2)
+{
+
+}
+
+address_relasi findElmRelasi(address_mhs L, int x)
+{
+
+}
+
+void printInfoRelasi(List_mhs P, List_mtkl L)
+{
+
+}
+
+void deleteRelasi(address_mhs P, address_mtkl L)
+{
+
+}
--- relasi.h
+++ relasi.h
@@ -1,46 +1,29 @@
 #ifndef RELASI_H_INCLUDED
 #define RELASI_H_INCLUDED
+#include "matkul.h"
+#include "mahasiswa.h"
 
 using namespace std;
+typedef struct elm_relasi *adddress_relasi;
 
-typedef struct elmlist_mhs *address_mhs;
-
-struct info_mhs{
+struct infotype_relasi{
     int nim;
     string nama;
-    string alamat;
-    string JK;
-    string jurusan;
-    string kelas;
+    int kode_mtkl;
+    string nama_mtkl;
 };
 
-
-struct elmlist_mhs{
-    info_mhs info;
-    address_mhs next;
+struct elm_relasi{
+    adddress_relasi next;
+    address_relasi prev;
+    infotype_relasi info;
 };
 
-typedef struct elmlist_mtkl *address_mtkl;
-
-struct info_mtkl{
-    int nim;
-    string nama;
-    string alamat;
-    string JK;
-    string jurusan;
-    string kelas;
-};
-
-
-struct elmlist_mtkl{
-    info_mtkl info;
-    address_mtkl next;
-};
-
-
-struct List_mhs{
-    address_mhs First;
-};
-
+void insertFirstRelasi(address_mhs P, address_relasi L, infotype_relasi R2);
+address_relasi alokasiRelasi( infotype_relasi C);
+void dealokasiRelasi(adddress_relasi &P);
+address_relasi findElmRelasi(address_mhs L, int x);
+void printInfoRelasi(List_mhs P, List_mtkl L);
+void deleteRelasi(address_mhs P, address_mtkl L);
 
 #endif // RELASI_H_INCLUDED
