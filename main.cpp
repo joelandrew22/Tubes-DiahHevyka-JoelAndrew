06aafba191ac89330d1f7ed06b461ec8a6d4339e
joelandrew22 <joel.andrew22088@gmail.com>
2017-03-21 17:20:35 +0700

    hampir selesai

--- main.cpp
+++ main.cpp
@@ -1,9 +1,131 @@
 #include <iostream>
+#include "matkul.h"
 
 using namespace std;
 
+void menu(List_mtkl &LC){
+    cout<<"PILIHAN MENU : "<<endl;
+    cout<<"1.Masukan data mahasiswa"<<endl;
+    cout<<"2.Masukan data matakuliah"<<endl;
+    cout<<"3.Tampilkan data mahasiswa"<<endl;
+    cout<<"4.Tampilkan data matakuliah"<<endl;
+    cout<<"5.Mencari data mahasiswa"<<endl;
+    cout<<"6.Mencari data matakuliah"<<endl;
+    cout<<"7.Gabungkan data mahasiswa dan matakuliah "<<endl;
+    cout<<"8.Tampilkan data hasil penggabungan"<<endl;
+    cout<<"9.Mencari data yang di gabungkan"<<endl;
+    cout<<"10.Hapus data yang di gabungkan"<<endl;
+    cout<<"11.Hapus data mahasiswa"<<endl;
+    cout<<"12.Hapus data matakuliah"<<endl;
+    cout<<"13.Urutkan data mahasiswa"<<endl;
+    cout<<"0.Menu utama"<<endl;
+    int pil;
+    cout<<"Masukan Pilihan : ";
+    cin>>pil;
+    address_mtkl P;
+    if(pil==1){
+            
+            }
+    else if(pil==2){
+        cout<<"Pilih data ingin di masukkan di bagian mana "<<endl;
+            cout<<"1.Pertama"<<endl;
+            cout<<"2.Setelah"<<endl;
+            cout<<"3.Terakhir"<<endl;
+            int men;
+            cout<<"Masukan pilihan : ";
+            cin>>men;
+            matakuliah Matakuliah;
+            if(men==1){
+                        cout<<"Masukan Kode Matakuliah : ";
+                        cin>>Matakuliah.kodematkul;
+                        cout<<"Masukan Nama Dosen : ";
+                        cin>>Matakuliah.namapengajar;
+                        cout<<"Banyak SKS : ";
+                        cin>>Matakuliah.sks;
+                        cout<<"Masukan Nama Matakuliah : ";
+                        cin>>Matakuliah.namamatkul;
+                        cout<<"Masukan ruangan : ";
+                        cin>>Matakuliah.ruangan;
+                        cout<<"Masukan Jam : ";
+                        cin>>Matakuliah.jam;
+                        address_mtkl P = alokasi(Matakuliah);
+                        insertFirst(LC, P);
+                        cout<<endl;
+                        cout<<"Ketik '0': ";
+                        cin>>men;
+                        if(men==0){
+                            menu(LC);
+                        }
+            }
+            else if(men==2){
+                        cout<<"Masukan Kode Matakuliah : ";
+                        cin>>Matakuliah.kodematkul;
+                        cout<<"Masukan Nama Dosen : ";
+                        cin>>Matakuliah.namapengajar;
+                        cout<<"Banyak SKS : ";
+                        cin>>Matakuliah.sks;
+                        cout<<"Masukan Nama Matakuliah : ";
+                        cin>>Matakuliah.namamatkul;
+                        cout<<"Masukan ruangan : ";
+                        cin>>Matakuliah.ruangan;
+                        cout<<"Masukan Jam : ";
+                        cin>>Matakuliah.jam;
+                        cout<<"Masukan setelah KODE Matakuliah : ";
+                        int x;
+                        cin>>x;
+                        address_mtkl Prec_C, P;
+                        Prec_C = findElm(LC, x); 
+                        P = alokasi(Matakuliah);
+                        insertAfter(LC, Prec_C, P);
+                        cout<<endl;
+                          cin>>men;
+                        if(men==0){
+                            menu(LC);
+                        }                                                          
+            }
+            else if(men==3){
+                        cout<<"Masukan Kode Matakuliah : ";
+                        cin>>Matakuliah.kodematkul;
+                        cout<<"Masukan Nama Dosen : ";
+                        cin>>Matakuliah.namapengajar;
+                        cout<<"Banyak SKS : ";
+                        cin>>Matakuliah.sks;
+                        cout<<"Masukan Nama Matakuliah : ";
+                        cin>>Matakuliah.namamatkul;
+                        cout<<"Masukan ruangan : ";
+                        cin>>Matakuliah.ruangan;
+                        cout<<"Masukan Jam : ";
+                        cin>>Matakuliah.jam;
+                        P = alokasi(Matakuliah);
+                        insertLast(LC, P);
+                        cout<<endl;
+                        cout<<"Ketik '0': ";
+                        cin>>men;
+                        if(men==0){
+                            menu(LC);
+                        }
+        }
+    }
+    else if(pil==3){
+
+    }
+    else if(pil==4){
+        printInfo(LC);
+            cout<<endl;
+            cout<<"Ketik '0': ";
+            int men;
+            cin>>men;
+            if(men==0){
+                menu(LC);
+            }    
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    List_mtkl LC;
+    menu(LC);
+
     return 0;
 }
+
--- matkul.cpp
+++ matkul.cpp
@@ -1,2 +1,148 @@
 #include <iostream>
 #include "matkul.h"
+
+void createList(List_mtkl &LC){
+    LC.First = NULL;
+    LC.Last = NULL;
+}
+
+address_mtkl alokasi(matakuliah x){
+    address_mtkl P;
+    P = new elmlist_mtkl;
+    P->info.kodematkul = x.kodematkul;
+    P->info.namapengajar = x.namapengajar;
+    P->info.sks = x.sks;
+    P->info.namamatkul = x.namamatkul;
+    P->info.ruangan = x.ruangan;
+    P->info.jam = x.jam;
+    P->next = NULL;
+    P->Prev = NULL;
+    return P; 
+}
+
+void dialokasi(address_mtkl &P){
+    delete P;
+}
+
+void insertFirst(List_mtkl &LC, address_mtkl P){
+    if (LC.First == NULL){
+        LC.First = P;
+        LC.First = P;
+    }else{
+        P->next = LC.First;
+        LC.First->Prev = P;
+        LC.First = P;
+    }
+}
+
+void insertLast(List_mtkl &LC, address_mtkl P){
+    if(LC.First == NULL){
+        insertFirst(LC, P);
+    }else{
+        LC.Last->next = P;
+        P->Prev = LC.Last;
+        LC.Last = P;
+    }
+}
+
+void insertAfter(List_mtkl &LC, address_mtkl Prec_C, address_mtkl P){
+    if(Prec_C == NULL){
+        if(Prec_C->next == NULL){
+            insertLast(LC, P);
+        }else{
+            P->next = Prec_C->next;
+            P->Prev = Prec_C;
+            Prec_C->next->Prev = P;
+            Prec_C->next = P; 
+        }
+    }else{
+        cout<<"Gagal insert after, Prec NULL"<<endl;
+    }
+}
+
+void deleteFirst(List_mtkl &LC, address_mtkl &P){
+    if(LC.First == NULL){
+        cout<<"No data"<<endl;
+    }else if(LC.First == LC.Last){
+        P = LC.First;
+        LC.First = NULL;
+        LC.Last = NULL;
+    }else{
+        P = LC.First;
+        LC.First = P->next;
+        LC.First->Prev = NULL;
+        P->next = NULL;
+    }
+}
+
+void deleteLast(List_mtkl &LC, address_mtkl &P){
+    if(LC.First == NULL){
+        cout<<"No data"<<endl;
+    }else if( LC.First== LC.Last){
+        P = LC.First;
+        LC.First = NULL;
+        LC.Last = NULL;
+    }else{
+        P = LC.Last;
+        LC.Last = P->Prev;
+        LC.Last->next = NULL;
+        P->Prev = NULL;
+    }
+}
+
+void deleteAfter(List_mtkl &LC, address_mtkl Prec_C, address_mtkl &P){
+    if(LC.First != NULL){
+        if(Prec_C != NULL && Prec_C != LC.Last){
+            if(Prec_C->next == LC.Last){
+                deleteLast(LC, P);
+            }else{
+                P = Prec_C->next;
+                Prec_C->next = P->next;
+                P->next->Prev = Prec_C;
+                P->Prev = NULL;
+                P->next = NULL;
+            }
+        }else{
+            cout<<"Gagal delete after"<<endl;
+        }
+    }else{
+        cout<<"Gagal delete after, list kosong"<<endl;
+    }
+}
+
+address_mtkl findElm(List_mtkl LC, int x){
+    address_mtkl P;
+    P = LC.First;
+    if (LC.First==NULL)
+    {
+        P=NULL;
+    }
+    else
+    {
+        while((P!=NULL)&&(P->info.kodematkul!=x))
+        {
+            P=P->next;
+        }
+    }
+    return P;
+}
+
+void printInfo(List_mtkl LC){
+    address_mtkl P;
+    P = LC.First;
+    if(LC.First ==NULL){
+        cout<<"List Kosong";
+    }
+    else{
+        while(P != NULL){
+            cout<<"Kode Matakuliah : "<<info(P).kodematkul<<endl;
+            cout<<"Nama Doesen : "<<info(P).namapengajar<<endl;
+            cout<<"Banyak SKS : "<<info(P).sks<<endl;
+            cout<<"Nama Matakuliah: "<<info(P).namamatkul<<endl;
+            cout<<"Ruangan : "<<info(P).ruangan<<endl;
+            cout<<"Jam masuk : "<<info(P).jam<<endl;
+            P = P->next;
+        }
+        cout<<endl;
+    }
+}
